414768f7f8924d487a4007584ca6e91e461e248c
Átila Goes <atilagoes.tech@gmail.com>
2025-09-23 21:19:31 -0300

    Adiciona validação de entrada e limita o tamanho do nome no formulário

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigosSorteio = [];
+function adicionarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nome = inputAmigo.value.trim();
+
+    if (nome === '') {
+        alert('Por favor, digite um nome válido!');
+        return;
+    }
+
+    if (nome.length > 30) {
+        alert('O nome deve ter no máximo 30 caracteres!');
+        return;
+    }
+
+    amigosSorteio.push(nome);
+    inputAmigo.value = '';
+    const listaAmigos = document.getElementById('listaAmigos');
+    const novoItem = document.createElement('li');
+    novoItem.textContent = nome;
+    listaAmigos.appendChild(novoItem);
+}
+
+function filtrarInput(event) { //validação para aceitar apenas letras (com ou sem acentos) e espaços utilizando regex
+    const input = event.target;
+    const regex = /^[a-zA-Zà-ÿ\s]*$/;
+    if (!regex.test(input.value)) {
+        input.value = input.value.replace(/[^a-zA-Zà-ÿ\s]/g, '');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const inputAmigo = document.getElementById('amigo');
+    inputAmigo.addEventListener('input', filtrarInput);
+});
